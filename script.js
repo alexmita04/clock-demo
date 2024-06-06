c700d1c3a6eb400614ff89963981b5da91a0ce0b
alexmita04 <alexmita04@gmail.com>
2024-06-06 13:17:02 +0300

    removed console.log statement

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function secondHandler() {
   minutesHand.style.transform = `translateY(150px) translateX(50px) rotate(${minutesDegrees}deg)`;
 
   const hours = date.getHours();
-  console.log(hours);
   const hoursDegrees = (hours / 12) * 360 + 9;
   hoursHand.style.transform = `translateY(150px) translateX(50px) rotate(${hoursDegrees}deg)`;
 }
